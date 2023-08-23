1eb2a7418453f1da93efb3dac810e4c0c0f5f91c
parth721 <ghoshparth630@gmail.com>
2023-08-23 20:53:00 +0530

    parse the json data

--- syncall/backend/views2.py
+++ syncall/backend/views2.py
@@ -12,7 +12,7 @@ class WeatherHistory(View):
         api_call = f'https://cloud.syncloop.com/tenant/1692162910856/packages.apiweather.weatherii.weatherhistory.main?lon=139&lat=35'
         headers = {
                     'Content-Type' : 'application/json',    
-                    'Authorization' : 'Bearer 6KPnAjasx2BzGyQSF2Lnfw51zqpeNjBy5lHN66E8Fgw5s8dxWsDmpr5DQQAOP7uBKhwOeYrBljpGnJKrpaUWEQ6h9JmFScSEHjiYZQ1NZXVIr+OTK6TXB2NWImPfVdzpwg033S0QyQ5jPJFnwwViuGFxW+QXqxoyJWY3h0Kago8G85t4pN6b3Vt88TjwMuK/BqG0RO2rP2H9mGa1GwrrEGbu0NbS5TPRj1XmVzmBHkJGVdkZKdFcF1u/b2h9TjytIJlyyCvbcFsSZNy8biSWe/dEXmQAH+37q3SuNecjjTVjnE+NexbNjPmpxkFurpBqcTJARvA0SJxE/4lzBNfKyfEOey0InePOX6UzcrQHWP7hYX83HbTQRNh0h5D0rfSu1FQaaSXpP3HMBlQKuKHwnQbwPmoDK1hdR+B92YPk+YC6kpDHnv5Eg/PY8ZMVwcr2c1ogeao+2XobkxUT5qQimo+7As8AD7Eb2lpCe375WXDCgFB6kzMViR7kYArHbfOXINPTWy+dOOLKzc37ode0gQ==',
+                    'Authorization' : 'Bearer {token}',
         }
 
         response = requests.get(api_call, headers=headers)
@@ -20,7 +20,24 @@ class WeatherHistory(View):
         #condition to check the API call is successful or not
         if response.status_code == 200:
             json_result = response.json()
-            return JsonResponse(json_result)
+            History_list = json_result['response']['jsonDoc']['result']
+
+            #Extract the required data from the API response
+            extracted_data = []
+            for history_entry in History_list:
+                mean_temp = history_entry['temp']['mean']
+                mean_pressure = history_entry['pressure']['mean']
+                mean_humidity = history_entry['humidity']['mean']
+                mean_clouds = history_entry['clouds']['mean']
+                mean_precipitation = history_entry['precipitation']['mean']
+                extracted_data.append({
+                    'mean_temp': mean_temp,
+                    'mean_pressure': mean_pressure,
+                    'mean_humidity': mean_humidity,
+                    'mean_clouds': mean_clouds,
+                    'mean_precipitation': mean_precipitation,
+                })
+            return JsonResponse({'weather_history' : extracted_data})
         
         else:
             error_message = {'error' : 'Something went wrong'}
