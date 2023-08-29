ad05c7d24443d338ece9d91be0d136425bb8d818
Partha Ghosh <112557191+parth721@users.noreply.github.com>
2023-08-29 14:47:09 +0530

    Update settings.py

--- syncall/syncall/settings.py
+++ syncall/syncall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iazb2n6i)6sly!1(zq1tkf0#9c#v+*)dudv9j036=ir@xkm7yn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static)
